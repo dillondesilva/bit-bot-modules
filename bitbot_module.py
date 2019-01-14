6f31020ef4f7bd41d1405eaa6ff4933cbef4660f
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-01-14 16:37:13 +1100

    Add files via upload
    Implemented spin_right() and spin_left() methods which allow the bot to spin on the respected axis at a certain speed

--- bitbot_module.py
+++ bitbot_module.py
@@ -1,20 +1,30 @@
 from microbit import *
 
+ANALOG_MAX = 1023
+
 # Converting values into a valid int between
 # 0-1023 for valid analog signals
 def to_analog(value):
-    analog_val = value * 1023
+    global ANALOG_MAX
+    analog_val = value * ANALOG_MAX
     return analog_val
-    
+
+# Checking that the user has entered a valid
+# speed before we can set the motors to that
+# speed. Raises an error if invalid speed is entered
+def check_speed(speed):
+    if speed < 0 or speed > 1:
+        raise ValueError('Invalid speed value for motors')
+        
+# Class for controlling Bit:bot motors    
 class Motors:
-    # accelerate() moves the bit:bot forward at a speed
+    # accelerate() moves the Bit:bot forward at a speed
     # set by the user. The speed must be between 0 - 1
     def accelerate(self, speed):
         # Checking that the user has entered a valid
         # speed before we can set the motors to that
         # speed
-        if speed < 0 or speed > 1:
-            raise ValueError('Invalid speed value for motors')
+        check_speed(speed)
         
         analog_val = to_analog(speed)
         pin0.write_analog(analog_val)
@@ -25,3 +35,18 @@ class Motors:
         pin0.write_digital(0)
         pin1.write_digital(0)
         
+    # spin_left() spins the Bit:bot left at a certain speed
+    def spin_left(self, speed):
+        check_speed(speed)
+        analog_val = to_analog(speed)
+        
+        pin0.write_analog(0)
+        pin1.write_digital(analog_val)
+        
+    # spin_right() spins the Bit:bot right at a certain speed
+    def spin_right(self, speed):
+        check_speed(speed)
+        analog_val = to_analog(speed)
+        
+        pin0.write_analog(analog_val)
+        pin1.write_digital(0)
