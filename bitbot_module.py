17cb637916518d87dd04ac8427c7714f59ecfeed
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-01-14 16:00:54 +1100

    Add files via upload
    Initial commit. Added source file. Began writing a class for Bit:bot motors which has a compatible forward() function which moves forward given a speed

---
+++ bitbot_module.py
@@ -0,0 +1,21 @@
+from microbit import *
+
+# Converting values into a valid int between
+# 0-1023 for valid analog signals
+def to_analog(value):
+    analog_val = value * 1023
+    return analog_val
+    
+class Motors:
+    # accelerate() moves the bit:bot forward at a speed
+    # set by the user. The speed must be between 0 - 1
+    def accelerate(self, speed):
+        # Checking that the user has entered a valid
+        # speed before we can set the motors to that
+        # speed
+        if speed < 0 or speed > 1:
+            raise ValueError('Invalid speed value for motors')
+        
+        analog_val = to_analog(speed)
+        pin0.write_analog(analog_val)
+        pin1.write_analog(analog_val)
